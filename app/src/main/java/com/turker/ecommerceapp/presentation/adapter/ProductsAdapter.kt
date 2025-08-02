2c40e2b5191a91be7a01f144c35a63d7f3e19bc6
EMRE <mr_turker@hotmail.com>
2025-08-02 16:12:30 +0300

    Add home screen ui structure

---
+++ app/src/main/java/com/turker/ecommerceapp/presentation/adapter/ProductsAdapter.kt
@@ -0,0 +1,80 @@
+package com.turker.ecommerceapp.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.turker.ecommerceapp.R
+import com.turker.ecommerceapp.data.model.ProductUI
+import com.turker.ecommerceapp.databinding.ProductItemBinding
+import com.turker.ecommerceapp.util.loadImage
+
+class ProductsAdapter(
+    private val productListener: ProductListener
+) : ListAdapter<ProductUI, ProductsAdapter.ProductViewHolder>(ProductDiffCallBack()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder =
+        ProductViewHolder(
+            ProductItemBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            productListener
+        )
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) =
+        holder.bind(getItem(position))
+
+    class ProductViewHolder(
+        private val binding: ProductItemBinding,
+        private val productListener: ProductListener
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(product: ProductUI) = with(binding) {
+            tvProductPrice.text = product.price.toString()
+            tvProductTitle.text = product.title
+            ivProduct.loadImage(product.imageOne)
+
+            var isFavorite = product.isFavorite
+
+            if (isFavorite) {
+                ivFavorite.setImageResource(R.drawable.ic_favorite_enable)
+            }
+
+            ivProduct.setOnClickListener {
+                productListener.onProductClick(product.id)
+            }
+
+            buttonAddToCart.setOnClickListener {
+                productListener.onAddToCartButtonClick(product.id)
+            }
+
+            ivFavorite.setOnClickListener {
+                isFavorite = !isFavorite
+                ivFavorite.apply {
+                    if (isFavorite) {
+                        ivFavorite.setImageResource(R.drawable.ic_favorite_enable)
+                    } else {
+                        ivFavorite.setImageResource(R.drawable.ic_favorite_disable)
+                    }
+                }
+                productListener.onFavoriteButtonClick(product)
+            }
+        }
+    }
+
+    class ProductDiffCallBack : DiffUtil.ItemCallback<ProductUI>() {
+        override fun areContentsTheSame(oldItem: ProductUI, newItem: ProductUI): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areItemsTheSame(oldItem: ProductUI, newItem: ProductUI): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+    interface ProductListener {
+        fun onProductClick(id: Int)
+        fun onAddToCartButtonClick(id: Int)
+        fun onFavoriteButtonClick(product: ProductUI)
+    }
+
+}
--- app/src/main/java/com/turker/ecommerceapp/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/com/turker/ecommerceapp/presentation/fragment/HomeFragment.kt
@@ -1,6 +1,97 @@
 package com.turker.ecommerceapp.presentation.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.turker.ecommerceapp.data.model.ProductUI
+import com.turker.ecommerceapp.databinding.FragmentHomeBinding
+import com.turker.ecommerceapp.presentation.adapter.ProductsAdapter
 
-class HomeFragment :  Fragment() {
+class HomeFragment : Fragment(), ProductsAdapter.ProductListener {
+
+    private lateinit var binding: FragmentHomeBinding
+    private val productsAdapter by lazy { ProductsAdapter(this) }
+    //private val viewModel by viewModels<HomeViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvProducts.adapter = productsAdapter
+
+        //viewModel.getAllProducts()
+        //viewModel.getFavoriteProducts()
+        observeData()
+    }
+
+    private fun observeData() {
+//        viewModel.homeState.observe(viewLifecycleOwner) { state ->
+//            when (state) {
+//                HomeState.Loading -> {
+//                    binding.progressBarHome.visible()
+//                }
+//
+//                is HomeState.ProductList -> {
+//                    productsAdapter.submitList(state.products)
+//                    binding.progressBarHome.invisible()
+//                }
+//
+//                is HomeState.SaleProductList -> {
+//                    discountsAdapter.submitList(state.saleProducts)
+//                    binding.progressBarHome.invisible()
+//                }
+//
+//                is HomeState.PostResponse -> {
+//                    binding.progressBarHome.invisible()
+//                    Toast.makeText(
+//                        requireContext(),
+//                        state.crud.message,
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                }
+//
+//                is HomeState.Error -> {
+//                    binding.progressBarHome.invisible()
+//                    Toast.makeText(
+//                        requireContext(),
+//                        state.throwable.message.orEmpty(),
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                }
+//            }
+//        }
+    }
+
+    override fun onProductClick(id: Int) {
+//        val direction = HomeFragmentDirections.homeToDetail(id)
+//        findNavController().navigate(direction)
+    }
+
+    override fun onAddToCartButtonClick(id: Int) {
+//        val addToCartRequest = AddToCartRequest(viewModelFirebase.currentUser!!.uid, id)
+//        viewModel.addToCart(addToCartRequest)
+    }
+
+    override fun onFavoriteButtonClick(product: ProductUI) {
+//        if (product.isFavorite) {
+//            viewModel.removeFromFavorite(product)
+//        } else {
+//            viewModel.addToFavorite(product)
+//        }
+//
+//        viewModel.getAllProducts()
+//        viewModel.getSaleProducts()
+    }
+
 }
